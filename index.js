3f140f9fef0f77e78b2e2d924f9084cafd9fc27e
pcsishun <pcsishun@Gmail.com>
2021-10-13 14:58:59 +0700

    update port

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const PORT =  8080;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+app.listen(PORT, ()=>console.log(`now run on port ${PORT}`));
